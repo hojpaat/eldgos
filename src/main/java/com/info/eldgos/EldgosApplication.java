db764d0fac986b7652147827cb08565b59c05d79
hojpaat <judit.of.horvath@gmail.com>
2024-11-11 21:23:51 +0000

    set up project

---
+++ src/main/java/com/info/eldgos/EldgosApplication.java
@@ -0,0 +1,13 @@
+package com.info.eldgos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EldgosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EldgosApplication.class, args);
+	}
+
+}
