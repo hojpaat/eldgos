7e482092d85fc10ebd5d5c94e794af9475789521
hojpaat <judit.of.horvath@gmail.com>
2025-07-03 19:47:23 +0100

    Introduce new fields in Volcano to align it with epos api response

--- src/main/java/com/info/eldgos/models/Volcano.java
+++ src/main/java/com/info/eldgos/models/Volcano.java
@@ -4,10 +4,10 @@ import java.time.LocalDate;
 import java.util.Arrays;
 import java.util.List;
 
-public record Volcano(String name, LocalDate date, String rockId) {
+public record Volcano(String name, LocalDate date, String rockId, Integer height, String area, Float lat, Float longt) {
     private static final List<Volcano> volcanoList = Arrays.asList(
-            new Volcano("Hekla", LocalDate.of(2024, 1, 1), "1"),
-            new Volcano("Katla", LocalDate.of(2021, 1, 1), "2")
+            new Volcano("Hekla", LocalDate.of(2024, 1, 1), "1", 1235, "Test area", 12.11f, 25.00f),
+            new Volcano("Katla", LocalDate.of(2021, 1, 1), "2", 555, "Another area", 12.25f, 12.45f)
     );
 
     public static Volcano getByName(String name) {
